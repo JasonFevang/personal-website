4ff1e7c8f58de44054a4e7c6aba63ae1958b558c
Jason <jasonfevang@gmail.com>
2022-01-30 23:35:45 -0800

    Completed gatsby tutorial

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,19 @@
 module.exports = {
     siteMetadata: {
-      title: `My Gatsby Site`,
-        siteUrl: `https://www.yourdomain.tld`,
+      title: `Jason Fevang`,
+        siteUrl: `https://www.fevang.ca`,
     },
     plugins: [
-
-    ]
-}
+      "gatsby-plugin-image",
+      "gatsby-plugin-sharp",
+      {
+        resolve: "gatsby-source-filesystem",
+        options: {
+          name: `blog`,
+          path: `${__dirname}/blog`,
+        }
+      },
+      "gatsby-plugin-mdx",
+      "gatsby-transformer-sharp",
+    ],
+};
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>For a good time call: 604-401-4643</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
