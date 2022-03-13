84a47a96ae8ac68dcf113a12662ffd7fc0a9281d
Jason Fevang <jasonfevang@gmail.com>
2022-03-13 09:16:27 -0700

    Improve stm8 blog post and debug the deploy script some more

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,18 @@ cd $(dirname "$0")
 source load_nvm
 
 # Build the gatsby site
+gatsby clean
 gatsby build
 
 # Verify it built correctly?
 
 # Copy the built site to my vps host
+# TODO: Also doesn't work, copying the public directory changes the owner. I need to copy the contents of the directory
 rsync -avz --delete public jason@fevang.ca:/home/jason/gatsby/my-gatsby-site
+
+# Reload nginx on the remote server
+# Didn't work, got the following error:
+# TODO: Allow interactive authentication with nginx
+  # Failed to reload nginx.service: Interactive authentication required.
+  # See system logs and 'systemctl status nginx.service' for details.
+systemctl --host jason@fevang.ca reload nginx
